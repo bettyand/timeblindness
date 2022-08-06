0f088437b1e2658901f5a0b2e71de203e35460a0
Betty Anderson <101119141+bettyand@users.noreply.github.com>
2022-08-05 20:14:04 -0400

    progress?

--- src/main/java/com/capstone/timeblindness/AppController.java
+++ src/main/java/com/capstone/timeblindness/AppController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AppController {
@@ -21,6 +22,9 @@ public class AppController {
 
     @Autowired
     private TaskService taskService;
+
+    @Autowired
+    private AttemptService attemptService;
 
     @GetMapping("/")
     public String viewHomePage() {
@@ -64,6 +68,45 @@ public class AppController {
     @GetMapping("/tasks")
     public String viewTasks(Model model) {
         model.addAttribute("listTasks", taskService.getAllTasks());
+        return "tasks";
+    }
+
+    @GetMapping("/tasks/{id}")
+    public String viewTask(@PathVariable("id") Long id, Model model) {
+        Task task = taskService.getTaskById(id);
+        model.addAttribute("task", task);
+        model.addAttribute("listAttempts", attemptService.getByTaskId(id));
+        return "view_task";
+    }
+
+    @GetMapping("/tasks/{id}/attempt")
+    public String newAttempt(@PathVariable("id") Long id, Model model) {
+        Task task = taskService.getTaskById(id);
+        Attempt attempt = new Attempt();
+        attempt.setTask(task);
+        model.addAttribute("task", task);
+        model.addAttribute("attempt", attempt);
+        return "new_attempt";
+    }
+
+    @PostMapping("/tasks/{id}/attempt/save")
+    public String saveAttempt(@ModelAttribute("attempt") Attempt attempt, @PathVariable("id") Long id, @RequestParam String action) {
+        if (action.equals("start")) {
+            attempt.start();
+            return "redirect:/tasks/" + id + "/attempt";
+        }
+
+        if (action.equals("stop")) {
+            attempt.stop();
+            return "redirect:/tasks/" + id + "/attempt";
+        }
+
+        if (action.equals("save")) {
+            attemptService.saveAttempt(attempt);
+            // add logic for updating task stats
+            return "redirect:/tasks/" + id + "?attemptsuccess";
+        }
+
         return "tasks";
     }
 
